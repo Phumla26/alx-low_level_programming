88d26ab9edad429136b3d27c95b490a9bcc0b982
Phumla26 <phumla.dladla1@gmail.com>
2023-04-27 14:51:19 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,7 +5,6 @@
 * Return: 0
 */
 
-
 int main(void)
 
 {
@@ -13,11 +12,13 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
 	i = 0;
 	/*while (i < 10)*/
 	/*{*/
 	/*	putchar(i);*/
 	/*}*/
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
