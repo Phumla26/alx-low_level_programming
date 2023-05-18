e1cb2de463c66fdda06328f6822545161e291d78
Phumla26 <phumla.dladla1@gmail.com>
2023-05-18 10:55:16 -0700

    task0

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,26 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _puts - prints a string to stdout
- * @str: pointer to the string to be printed
- *
- * Return: void
+ * _puts - prints a string, followed by a new line,stdout
+ * @str: string to print
  */
+
 void _puts(char *str)
-{
-    /**
-     * Function implementation goes here
-     */
-}
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+while (*str != '\0')
+{
+_putchar(*str++);
+}
+_putchar('\n');
 }
